07143100e35c86341545ad835ce568f99743922d
ngjy <104226068@qq.com>
2021-04-02 12:30:31 +0800

    C语言实现三子棋

---
+++ 三子棋/game/game.h
@@ -0,0 +1,13 @@
+#define ROW 3
+#define COL 3
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//ÉùÃ÷
+void InitBoard(char board[ROW][COL], int row, int col);
+void DisplayBoard(char board[ROW][COL], int row, int col);
+void PlayerMove(char board[ROW][COL], int row, int col);
+void ComputerMove(char board[ROW][COL], int row, int col);
+char IsWin(char board[ROW][COL], int row, int col);
